2709d26ce051ed57f004cfbf4877d3dc79fba13c
nooralrawashdah <nooralrawashdah5@gmail.com>
2026-04-03 13:52:17 +0300

    setup cors and update dependencies

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const usersRouter = require("./routes/user");
 const productsRouters = require("./routes/product");
 const categoryRoutes= require("./routes/category");
+const orderRoutes= require("./routes/order")
+const cors=require("cors");  
 require("./models/db");
 const app = express();
 app.use(express.static("public")); 
@@ -12,9 +14,12 @@ app.get("/health",(req,res)=>{
     res.send("OK");
 })*/
 //console.log(process.env.test);
+app.use(cors({ origin: "http://localhost:3000"}  ))
 app.use("/users",usersRouter);
 app.use("/products",productsRouters);
 app.use("/category",categoryRoutes);
+app.use("/order",orderRoutes);
+
 app.listen(process.env.PORT, () => {
 console.log(`Server running at http://localhost:${process.env.PORT}`);
 });
